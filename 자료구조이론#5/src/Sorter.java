b715234f60ae2411c7de0bd3b172ed7fb1fad962
Wa4B <ghd1876@naver.com>
2015-11-27 20:20:02 +0900

    1127-0

--- 자료구조이론#5/src/Sorter.java
+++ 자료구조이론#5/src/Sorter.java
@@ -10,6 +10,23 @@ public class Sorter {
 	public static void selectionSort(int[] entry) {
 		// Your code goes here!
 		
+		int temp = 0;
+		int min = 0;
+		
+		for(int i = 0 ; i < entry.length ; i++){
+			min = 0;
+			for(int j = i+1 ; j <entry.length ; j++){
+				if(entry[j]<entry[min]){
+					min = j;
+				}
+			}
+			temp = entry[i];
+			entry[i] = entry[min];
+			entry[min] = temp;
+			System.out.println(entry[i]);
+		}
+		
+		
 	}
 	
 	public static void insertionSort(int[] entry) {
