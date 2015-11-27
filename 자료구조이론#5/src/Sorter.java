187c03791890d12565a3f7be380cdddbf018d7f8
Wa4B <ghd1876@naver.com>
2015-11-27 20:49:43 +0900

    1127-1

--- 자료구조이론#5/src/Sorter.java
+++ 자료구조이론#5/src/Sorter.java
@@ -31,11 +31,29 @@ public class Sorter {
 	
 	public static void insertionSort(int[] entry) {
 		// Your code goes here!
-		
+		int temp;
+		int set;
+		for(int i = 1 ; i < entry.length; i++){
+			set = i;
+			
+			for(int j = i-1; j > -1; j--){
+				if(entry[j]>entry[set]){
+					entry[j+1] += entry[j];
+				}else{
+					entry[j] = entry[set];
+					break;
+				}
+			}
+			System.out.println(entry[i]);
+		}
 	}
+	
+	
 	
 	public static void heapSort(int[] entry) {
 		// Your code goes here!
+		int[] heaps = new int[entry.length];
+		
 		
 	}
 }
